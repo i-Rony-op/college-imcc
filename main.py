eafa5fa4d32277bfc91a2b204166e775e5644fee
Raunak Sharma <raunaksharma6205@gmail.com>
2026-09-10 13:36:16 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# num = int(input("Enter a number: "))
+# cal = num%2
+# print(f"Number is Odd: {bool(cal)}")
+
+
+# age = int(input("Enter your age: "))
+# print(f"{age}years = {age*365} days")
+
+
+# minutes = int(input("Enter Minutes: "))
+# print(f"{minutes} minutes is {minutes // 60}hours {minutes%60}minutes ")
+
+# num = int(input("Enter Number: "))
+# print(f"{num} : last digit is {num%10}")
+
+role = input("Enter Role: ")
+age = int(input("Enter Age: "))
+
+role == "Student" and age < 21
+print(f"Eligible : {role.lower() == "student" and age < 21}")
